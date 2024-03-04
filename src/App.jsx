8cefd86c270110ad0bf351bbc651669f01ccf48c
thesteelknight <crismaster250@gmail.com>
2024-03-03 22:54:24 -0500

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
 
 export default function App() {
   return (
-    <div>
-      XAGA Company
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
